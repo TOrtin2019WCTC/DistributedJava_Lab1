936628c39c497fe705971f0c0fcf188e406b940e
tortin2019wctc <tortin@my.wctc.edu>
2019-08-26 15:33:45 -0500

    Changed the script

--- apex-script.js
+++ apex-script.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 'use strict';
 
+var firstName;
+
 $('#newsletter').on('submit', function(e){
     e.preventDefault();
+
+    firstName = $('#fName').val();
+    $('#confirmation').html('Success, ' + firstName + '!' + ' Your info has been saved.<br><br>' +
+        'When a new Legend becomes playing in Apex Legends, you will receive an email detailing their abilities.');
     $('#confirmation').show();
    $('#form1').hide();
-  
+
 })
 });
