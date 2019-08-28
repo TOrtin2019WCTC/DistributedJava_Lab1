bfce538a6319ebbbe8396551a5df3cad0bf90ed4
tortin2019wctc <tortin@my.wctc.edu>
2019-08-28 14:10:55 -0500

    changed form appearance

--- apex-script.js
+++ apex-script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
                 email: {
                     required: true,
                     email: true
-                    
+
                 }
             },
             messages: {
